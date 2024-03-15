5aff270dd23904598e7c55d433bcd8d2892e50b9
ho-beom-cheon <cjs414ghqja@gmail.com>
2024-03-15 10:49:18 +0900

    #1 init

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img class="logo" src="./assets/logo.svg" alt="Vue logo" />
+    <img class="logo vue" src="./assets/logo-vue.svg" alt="Vue logo" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
 .logo {
   height: 6em;
-  padding: 1.5em;
+  padding: 1.5em;`
   will-change: filter;
   transition: filter 300ms;
 }
